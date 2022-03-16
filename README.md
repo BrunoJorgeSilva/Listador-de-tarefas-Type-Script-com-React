ecbf103c6fd35940df78c1e2cbe8c3dc87f2f1ae
isadorinha <war25war2525@gmail.com>
2022-03-16 16:15:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Objetivo e Como Iniciar o Projeto 
+Download do zip, extrair os arquivos na pasta desejada, abrir o terminal dentro da pasta e acionar o comando NPM start. 
+O aplicativo vai rodar no seu servidor local. 
+Toda vez que for iniciado ele inicia sem dados, excelente aplicação para uso diário. 
+
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import {useState} from 'react'; 
+import * as C from './App.styles'; 
+import {Item} from './types/Item';
+import {ListItem} from './components/ListItem'; 
+import { AddArea } from './components/AddArea';
+
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+     
+  ]); 
+
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list]; 
+    newList.push({
+      id: list.length + 1, 
+      name: taskName,
+      done: false
+    });
+    setList(newList); 
+  }
+  return (
+    <C.Container>
+        <C.Area>
+        <C.Header>Lista de Tarefas Diárias</C.Header>
+
+        <AddArea onEnter={handleAddTask} />
+
+        {list.map((item, index )=>(
+          <ListItem key={index} item={item} />
+        ))}
+
+        </C.Area>
+      </C.Container>
+  );
+}
+
+export default App; 
